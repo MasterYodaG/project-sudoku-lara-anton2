e14215410345670374d60a221eb9337f006ca3f3
Rakiiia <Rakiiia@mail.ru>
2025-08-08 16:37:54 +0300

    end

--- runner.js
+++ runner.js
@@ -1,1 +1,16 @@
+// Подключаем нужные функции из sudoku.js
 const { readAll, solve, isSolved, prettyBoard } = require('./sudoku.js');
+
+// Читаем все пазлы из файла puzzles.txt
+const boards = readAll('./puzzles.txt');
+
+// Берём первый пазл (можно менять индекс)
+const board = boards[0];
+
+// Решаем и выводим результат
+if (solve(board)) {
+  prettyBoard(board);
+  console.log('Решено?', isSolved(board));
+} else {
+  console.log('Решение не найдено');
+}
